1adebeb77d1495d7d214bd16af896c3e5d349bd7
Aliaksandr Aliaksandrau <aliaksandra@playtika.com>
2021-11-28 18:25:00 +0300

    test update

--- application/setup.py
+++ application/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='demo',
-    version='0.2',
+    version='0.2.1',
     long_description=__doc__,
     packages=['demo'],
     include_package_data=False,
